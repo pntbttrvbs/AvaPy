daef184f322bd56288c63a2290000e77c50fe647
Jaime (Justin) <justinspott@gmail.com>
2020-05-08 11:57:04 -0600

    Update HelloSpectrometer.py
    updated example from old wrapper to AvaPy

--- HelloSpectrometer.py
+++ HelloSpectrometer.py
@@ -1,34 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 29 21:57:20 2020
-
-@author: justins
-"""
-
-#start - what do I need to talk to the spectrometer?
-
-#import wrapper
 from avaspec import *
-
-#connect to the .dll
 
 #(-1 Eth and USB, 0 USB, 256 Eth)
 ret = AVS_Init(-1)
-#or depricated AVS_GetNrOfDevices
-ret = AVS_GetNrOfDevices()
-#ret = AVS_UpdateUSBDevices()
-#why 75? explain how to find this.
-#lib = ctypes.CDLL("avaspecx64.dll")
-#init; updateUSBDevices
-#Param1 = 
-#AVS_GetList()
-ret = AVS_GetList(75, 75, AvsIdentityType*1)
-handle = AVS_Activate(ret[1])
+ret = AVS_UpdateUSBDevices()
+ret = AVS_GetList()
+handle = AVS_Activate(ret[0])
 
 config = DeviceConfigType
-ret = AVS_GetParameter(handle, 63484, 63484, config)
-pixels = ret[1].m_Detector_m_NrPixels
-lamb = AVS_GetLambda(handle,[])
+ret = AVS_GetParameter(handle)
+pixels = AVS_GetNumPixels(handle)
+lamb = AVS_GetLambda(handle)
 wavelengths = []
 for pix in range(pixels):
     wavelengths.append(lamb[pix])
@@ -63,12 +44,10 @@ while not dataready:
     dataready = AVS_PollScan(handle)
     time.sleep(measconfig.m_IntegrationTime/1000)
         
-timestamp = 0
-spectraldata = []
-ret = AVS_GetScopeData(handle, timestamp, spectraldata)
-timestamp = ret[0]
+spectrum = []        
+timestamp, scopedata = AVS_GetScopeData(handle)
 for i,pix in enumerate(wavelengths):
-    spectraldata.append(ret[1][i])
+    spectrum.append(scopedata[i])
 
 import matplotlib.pyplot as plt
-plt.plot(wavelengths,spectraldata)
+plt.plot(wavelengths,spectrum)
